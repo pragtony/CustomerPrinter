e28ac9fd22b833a8feec3a11a71c463a70de97c3
pragtony <bitbucket4444@gmail.com>
2017-05-15 18:32:39 +0200

    initial checkin CustomPrinter

---
+++ CustomPrinter/src/com/github/pragtony/Main.java
@@ -0,0 +1,8 @@
+package com.github.pragtony;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Printer.displayMsg("Test");
+	}
+}
---
+++ CustomPrinter/src/com/github/pragtony/Printer.java
@@ -0,0 +1,7 @@
+package com.github.pragtony;
+
+public class Printer {
+	public static void displayMsg(String s){
+		System.out.println(s);
+	}
+}
